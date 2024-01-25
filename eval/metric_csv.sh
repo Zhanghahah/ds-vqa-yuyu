776b93df4e114f555d127e0709625ffbe3823f65
Zhanghahah <572709035@qq.com>
2024-01-25 11:16:04 +0800

    add execute sh

---
+++ eval/metric_csv.sh
@@ -0,0 +1,3 @@
+python -u metric_cal.py \
+    --dataset LSVQ_1080p \
+    --in_file /data/zhangyu/ds-vqa-yuyu/eval/results/LSVQ_whole_test_1080p_ds/pred_DATE01-19_Epoch6_LR1e-3_data_lsvq_align_all_1_epoch-1.csv
