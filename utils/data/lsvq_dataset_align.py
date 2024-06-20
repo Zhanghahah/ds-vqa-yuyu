8ed486e5b566ad3363657518e8e4fc9296669c44
Zhanghahah <572709035@qq.com>
2024-06-21 00:10:37 +0800

    update lsvq_dataset_align

--- utils/data/lsvq_dataset_align.py
+++ utils/data/lsvq_dataset_align.py
@@ -41,7 +41,7 @@ def load_and_transform_video(video_path,
         video_fps = vr.get_avg_fps()
         sample_gap = round(video_fps / sample_fps)
         sample_frame_idx = [i for i in range(0, total_frame_num, sample_gap)]
-        video_data = vr.get_batch(sample_frame_idx).asnumpy()   # (f, h, w, 3)
+        video_data = vr.get_batch(sample_frame_idx).asnumpy()  # (f, h, w, 3)
         frame_time = [x / video_fps for x in sample_frame_idx]
 
         # decord.bridge.set_bridge('torch')
@@ -58,21 +58,21 @@ def load_and_transform_video(video_path,
         if duration == 0:
             frame_id_list = np.array([0])
         else:
-            frame_id_list = np.linspace(0, duration-1, duration, dtype=int)
+            frame_id_list = np.linspace(0, duration - 1, duration, dtype=int)
 
         video_data = []
-        print_rank_0(f"Processing video {video_path}, frame_id_list: {frame_id_list}")
+        print_rank_0(f"Processing video {video_path}, the num of frame_id_list: {len(frame_id_list)}")
         for frame_idx in tqdm(frame_id_list):
             cv2_vr.set(cv2.CAP_PROP_POS_FRAMES, frame_idx - 1)
             ret, frame = cv2_vr.read()
-
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            if frame is None:
-                continue
-            # try:
+            try:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            except:
+                if frame is None:
+                    continue
+                # try:
             #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             # except:
-            #
             #     while frame is None:
             #         random_frame_idx = np.random.choice((duration - 1) // 2)
             #         cv2_vr.set(cv2.CAP_PROP_POS_FRAMES, random_frame_idx)
@@ -84,7 +84,9 @@ def load_and_transform_video(video_path,
             video_data.append(frame)
         cv2_vr.release()
         assert len(video_data) != 0, "We need at least one frame for feature extraction."
-        # video_data = torch.stack(video_data, dim=1)
+        # if len(video_data) == 0:
+        #     """we need to give a dummy video frame for negative smaple learning"""
+
     else:
         raise NameError('video_decode_backend should specify in (pytorchvideo, decord, opencv)')
     return video_data
@@ -97,13 +99,20 @@ class LSVQAlignDataset(VQADataset):
                  video_loader_type,
                  tokenizer,
                  vis_processor,
-                 add_eos=True, ignore_instruction=True, save_video_feat=False, **kwargs):
+                 add_eos=True, ignore_instruction=True, save_video_feat=True, **kwargs):
         vis_root = f"{data_path}/LSVQ/LSVQ"
         feature_root = "/data1/zhangyu/own_data/VQA/LSVQ/LSVQ_feature_1fps"
-        assert os.path.isdir(vis_root), f"LSVQAlignDataset image directory {vis_root} not found, you need to check the image path"
+        motion_root = "/data1/zhangyu/own_data/VQA/LSVQ/LSVQ_SlowFast2_feature"
+        assert os.path.isdir(
+            vis_root), f"LSVQAlignDataset image directory {vis_root} not found, you need to check the image path"
 
-        ann_paths = ["LSVQ/LSVQ/a_split_metadata/LSVQ_whole_test_ds_score.json", "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_train_ds_score.json",
-                     "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_test_1080p_ds.json"]
+        ann_paths = [
+            "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_train_ds_score.json",
+            "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_test_ds_score.json",
+            "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_train_ds_class.json",
+            "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_test_ds_class.json",
+            "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_test_1080p_ds_class.json",
+            "LSVQ/LSVQ/a_split_metadata/LSVQ_whole_test_1080p_ds.json"]
         q_mos_path = os.path.join(data_path, 'a_prompt/prompt_list_noTask.json')
         q_ass_path = os.path.join(data_path, 'a_prompt/prompt_list_noTask_ass.json')
 
@@ -113,17 +122,27 @@ class LSVQAlignDataset(VQADataset):
         self.feature_root = feature_root
         self.save_video_feat = save_video_feat
 
+        self.motion_root = motion_root
+        self.ext_motion_token = True
+
         real_ann_paths = []
         for ann_path in ann_paths:
             ann_path = f"{data_path}/{ann_path}"
             real_ann_paths.append(ann_path)
-            assert os.path.isfile(ann_path), f"LSVQAlignDataset annotation file {ann_path} not found, you need to identify it from your folder"
+            assert os.path.isfile(
+                ann_path), f"LSVQAlignDataset annotation file {ann_path} not found, you need to identify it from your folder"
         super().__init__(data_path, data_debug_path, per_sample_image, tokenizer, vis_processor,
                          vis_root, real_ann_paths, annotation_key="annotations", **kwargs)
 
+    def motion_feature_encode(self, slow_mo_feat_name, fast_mo_feat_name):
+
+        slow_mo_feat = torch.from_numpy(np.load(slow_mo_feat_name)).squeeze().cuda()
+        fast_mo_feat = torch.from_numpy(np.load(fast_mo_feat_name)).squeeze().cuda()
+        feat_3D = torch.cat([slow_mo_feat, fast_mo_feat]).unsqueeze(0)
+        return feat_3D
+
     def process_text(self, ann, data_debug_path=None, data_debug_counter=0, first_message=True):
         # random select a question
-
         if ann['ann_type'] == 'score':
             question = random.choice(self.Q_MOS)
             answer = ann["score"]
@@ -141,11 +160,45 @@ class LSVQAlignDataset(VQADataset):
             if not os.path.exists(feature_path):
                 feature_path = os.path.join(self.feature_root, ann["image_id"] + ".mp4")
             image_list = []
-            for img_feat_file in os.listdir(feature_path):
+            motion_feat_list = []
+            # find the max motion token
+            max_motion_idx = sorted(os.listdir(os.path.join(self.motion_root, ann["image_id"])))[-1].split('_')[1]
+            feat_path_list = sorted(os.listdir(feature_path))
+            fs_count = len(feat_path_list)
+            if fs_count < 8:
+                feat_path_list += [feat_path_list[-1]] * (8 - fs_count)
+            elif fs_count > 8:
+                feat_path_list = feat_path_list[:8]
+            for img_feat_file in feat_path_list:
+                img_index = img_feat_file.split('.')[0]
+                # if img_index > max_motion_idx:c
+                #     continue
+                if self.ext_motion_token:
+                    # [1,2304]
+                    slow_mo_feat = os.path.join(self.motion_root, ann["image_id"],
+                                                f"feature_{img_index}_slow_feature.npy")
+                    fast_mo_feat = os.path.join(self.motion_root, ann["image_id"],
+                                                f"feature_{img_index}_fast_feature.npy")
+
+                    if not os.path.exists(slow_mo_feat) or not os.path.exists(fast_mo_feat):
+                        slow_mo_feat = os.path.join(self.motion_root, ann["image_id"],
+                                                    f"feature_{max_motion_idx}_slow_feature.npy")
+                        fast_mo_feat = os.path.join(self.motion_root, ann["image_id"],
+                                                    f"feature_{max_motion_idx}_fast_feature.npy")
+                    motion_feat_per_img = self.motion_feature_encode(slow_mo_feat, fast_mo_feat)
+                    motion_feat_list.append(motion_feat_per_img)
                 img_feat = torch.load(os.path.join(feature_path, img_feat_file)).unsqueeze(0)
                 image_list.append(img_feat)
-            images = torch.cat(image_list, 0).mean(0)
-            return images
+
+            if self.ext_motion_token:
+                # if we add motion token for each image, we return two list: image_list and motion_list
+                # img_mo_feats_list = [torch.cat([img_feat, mo_feat], dim=0)
+                #                      for img_feat, mo_feat in zip(image_list, motion_feat_list)]
+                return [torch.cat(image_list, 0), torch.cat(motion_feat_list, 0)]
+            else:
+                # if we do not use motion token, we just return a image tensor
+                images = torch.cat(image_list, 0).mean(0)
+                return images
         else:
             video_path = os.path.join(self.vis_root, ann["image_id"] + ".mp4")
             video_data = load_and_transform_video(video_path,
